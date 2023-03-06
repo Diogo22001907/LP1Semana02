f00103135b0433241ea9d018c19f88561e91ccb5
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-06 20:01:01 +0000

    Variables Created

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the height : ");
+            string height_input = Console.ReadLine();
+            Console.WriteLine("Now please enter the radius : ");
+            string radius_input = Console.ReadLine();
+
+            int height;
+            int radius;
+            double pi = 3.1415926;
+
+            
+        }
+    }
+}
