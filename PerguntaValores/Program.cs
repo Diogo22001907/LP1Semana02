746b96d6f7d8fd37010ca16056568ff0f40c1fc7
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-06 21:18:02 +0000

    Added volume equation

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -10,12 +10,26 @@ namespace PerguntaValores
             string height_input = Console.ReadLine();
             Console.WriteLine("Now please enter the radius : ");
             string radius_input = Console.ReadLine();
-
-            int height;
-            int radius;
+            
+            double height;
+            double radius;
             double pi = 3.1415926;
 
-            
+            if (double.TryParse(height_input, out height) && (double.TryParse(radius_input, out radius)))
+            {
+                double volume = pi * Math.Pow(radius, 2) * height;
+                Console.WriteLine("The volume of the cilinder is " + volume);
+            }
+
+            else
+            {
+                Console.WriteLine("The input given are not correct.");
+            }
+
+
         }
     }
 }
+
+
+
