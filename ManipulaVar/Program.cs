dac52a0b995698677caf947e1d80aef2ed544a60
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-06 19:26:17 +0000

    Exercise 1 completed

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           if (args.Length > 0)
+            {
+                int x;
+                if (int.TryParse(args[0], out x))
+                {
+                    x ++;
+                    Console.WriteLine(x);
+                    --x;
+                    --x;
+                    Console.WriteLine(x);
+                }
+                else
+                {
+                    Console.WriteLine("The input given is not valid for this operation");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter an input");
+            }
+        }
+    }
+}
