572ca87498a4e0b2fe63b1bd323ee5888233772f
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-08 13:01:41 +0000

    Completed Exercise 5 and other fixes

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -6,23 +6,23 @@ namespace PerguntaValores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the height : ");
+            Console.WriteLine("Please enter the height in centimeters : ");
             string height_input = Console.ReadLine();
-            Console.WriteLine("Now please enter the radius : ");
+            Console.WriteLine("Now please enter the radius in centimeters : ");
             string radius_input = Console.ReadLine();
             
             double height;
             double radius;
-            double pi = 3.1415926;
+            
 
             if (double.TryParse(height_input, out height) && (double.TryParse(radius_input, out radius)))
             {
                 /* Added the limit of two decimal houses to the results of both cylinder volume and the area of surface.
                 The previous push to the repository has the results without the decimal houses limit.*/
-                double volume = pi * Math.Pow(radius, 2) * height;
-                double area_surface = 2 * pi * radius * (radius + height);
-                Console.WriteLine("The volume of the cylinder is '{0:f2}'", volume);
-                Console.WriteLine("The area surface of the cylinder is '{0:f2}'", area_surface);
+                double volume = Math.PI * Math.Pow(radius, 2) * height;
+                double area_surface = 2 * Math.PI * radius * (radius + height);
+                Console.WriteLine("The volume of the cylinder is {0:f2} cm", volume);
+                Console.WriteLine("The area surface of the cylinder is {0:f2} cm", area_surface);
             }
 
             else
