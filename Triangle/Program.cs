e0a390a6031924afea7c4cf778c0777074b0da4a
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-08 22:57:56 +0000

    Exercise 10 completed

---
+++ Triangle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                int lines = 1;
+                int num;
+                string letter;
+
+                if (int.TryParse(args[0], out num))
+                {
+                    letter = args[1];
+                }
+
+                else if (int.TryParse(args[1], out num))
+                {
+                    letter = args[0];
+                }
+
+                else
+                {
+                    Console.WriteLine("User input incorrect");
+                    return;
+                }
+
+                while (lines <= num)
+                {
+                    Console.WriteLine(new String(letter[0], lines));
+                    lines++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("User input incorrect");
+            }
+        }
+    }
+}
