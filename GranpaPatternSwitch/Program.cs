96b82849823d9c384166144015f0b7e4d283aedb
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-08 20:37:10 +0000

    Changed the syntax pattern for the switch expression

---
+++ GranpaPatternSwitch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GranpaPatternSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Colocar questão ao utilizador
+            Console.Write("Place your question? ");
+            string question = Console.ReadLine();
+
+
+            string response = question switch
+            {
+                "How are you ?" => "I`m fine , thank you",
+                "What`s your name ?" => "Gran ` pa",
+                "what`s your mission ?" => "Destroy Mankind",
+                _ => "You got me , i am not that smart",
+
+
+            };
+
+
+            Console.WriteLine(response);
+
+
+        }
+    }
+}
