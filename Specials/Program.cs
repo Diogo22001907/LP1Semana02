a3730aea56ae03e7663b46559c75ce9266cad82c
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-08 18:33:58 +0000

    Added max and min values of several variables

---
+++ Specials/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Specials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //integer
+            int i = int.MaxValue;
+            int i2 = int.MinValue;
+            Console.WriteLine(i);
+            Console.WriteLine(i2);
+
+            //float
+            float f = float.MaxValue;
+            float f2 = float.MinValue;
+            Console.WriteLine(f);
+            Console.WriteLine(f2);
+
+            //double
+            double d = double.MaxValue;
+            double d2 = double.MinValue;
+            Console.WriteLine(d);
+            Console.WriteLine(d2);
+
+            //long
+            long l = long.MaxValue;
+            long l2 = long.MinValue;
+            Console.WriteLine(l);
+            Console.WriteLine(l2);
+
+            //short
+            short s = short.MaxValue;
+            short s2 = short.MinValue;
+            Console.WriteLine(s);
+            Console.WriteLine(s2);
+
+            //sbyte
+            sbyte sb = sbyte.MaxValue;
+            sbyte sb2 = sbyte.MinValue;
+            Console.WriteLine(sb);
+            Console.WriteLine(sb2);
+
+            //ulong
+            ulong ul = ulong.MaxValue;
+            ulong ul2 = ulong.MinValue;
+            Console.WriteLine(ul);
+            Console.WriteLine(ul2);
+
+            //byte
+            byte b = byte.MaxValue;
+            byte b2 = byte.MinValue;
+            Console.WriteLine(b);
+            Console.WriteLine(b2);
+
+
+        }
+    }
+}
